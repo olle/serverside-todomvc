515f6da36b19d8f31de9d58e56b16d6957f4a44d
Olle Törnström <olle@studiomediatech.com>
2013-10-30 23:45:51 +0100

    No compile errors
    * But everything else is pretty ca-ca. Man-up and take it from
      here, reimplement the Wicket skeleton and get something up
      and going.

--- java-examples/serverside-todomvc-wicket/src/main/java/com/studiomediatech/TodoMVC.java
+++ java-examples/serverside-todomvc-wicket/src/main/java/com/studiomediatech/TodoMVC.java
@@ -1,12 +1,7 @@
 package com.studiomediatech;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-
-import com.studiomediatech.serverside.todomvc.common.storage.GuavaCacheStorage;
+import com.studiomediatech.serverside.todomvc.common.storage.Repository;
 import com.studiomediatech.serverside.todomvc.common.storage.Storage;
-import com.studiomediatech.serverside.todomvc.common.storage.Store;
 
 import org.apache.wicket.Session;
 import org.apache.wicket.markup.head.IHeaderResponse;
@@ -19,29 +14,22 @@ public class TodoMVC extends WebApplication {
 
   public static final String JS_BUCKET = "foot";
 
-  private Storage<Todo, String> storage;
+  private final Storage<Todo, String> storage;
 
   public TodoMVC() {
-    this.storage = Storage.<Todo, String> newGuavaCacheStorage();
+    this.storage = Storage.newGuavaCacheStorage();
 
-    this.storage = new GuavaCacheStorage<Todo>(new Callable<List<Todo>>() {
-      @Override
-      public List<Todo> call() throws Exception {
-        ArrayList<Todo> todos = new ArrayList<Todo>();
-        Todo todo = new Todo("Implement TodoMVC in a new framework");
-        // TODO: Solve some other way!
-        // todo.setId(Integer.toString(todo.hashCode()));
-        todos.add(todo);
-        return todos;
-      }
-    });
+    // TODO: Provide a way to pass default values to storage.
+    // ArrayList<Todo> todos = new ArrayList<Todo>();
+    // Todo todo = new Todo("Implement TodoMVC in a new framework");
+    // todos.add(todo);
   }
 
-  public static Store<Todo> getStorage(Session session) {
+  public static Repository<Todo, String> getStorage(Session session) {
     return ((TodoMVC) get()).getSessionStorage(session);
   }
 
-  private Store<Todo> getSessionStorage(Session session) {
+  private Repository<Todo, String> getSessionStorage(Session session) {
     return this.storage.forKey(session.getId());
   }
 
--- java-examples/serverside-todomvc-wicket/src/main/java/com/studiomediatech/Todos.java
+++ java-examples/serverside-todomvc-wicket/src/main/java/com/studiomediatech/Todos.java
@@ -2,23 +2,23 @@ package com.studiomediatech;
 
 import java.util.List;
 
-import org.apache.wicket.markup.repeater.data.ListDataProvider;
-
 import com.google.common.collect.Lists;
 import com.studiomediatech.serverside.todomvc.common.storage.Repository;
 
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
+
 public class Todos extends ListDataProvider<Todo> {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private final Repository<Todo> storage;
+  private final Repository<Todo, String> storage;
 
-    public Todos(Repository<Todo> storage) {
-        super();
-        this.storage = storage;
-    }
+  public Todos(Repository<Todo, String> storage) {
+    super();
+    this.storage = storage;
+  }
 
-    @Override
-    protected List<Todo> getData() {
-        return Lists.newLinkedList(this.storage.findAll());
-    }
+  @Override
+  protected List<Todo> getData() {
+    return Lists.newLinkedList(this.storage.findAll());
+  }
 }
