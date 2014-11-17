ce2394f8977be1affea4143c867efbaa42501be0
Olle Törnström <olle@studiomediatech.com>
2014-11-17 21:09:35 +0100

    Add task type and toggling of status
    * Adds a small task type, with the capability to toggle status.

--- examples-python-flask/todomvc.py
+++ examples-python-flask/todomvc.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 import uuid
 
+class Task:
+    def __init__(self, task, status='active'):
+        self.id = uuid.uuid1().hex
+        self.task = task
+        self.status = status
+
+    def toggle(self):
+        if self.status == 'active':
+            self.status = 'completed'
+        else:
+            self.status = 'active'
+
 # Global state, yay!
 tasks = {}
 
@@ -13,12 +25,18 @@ def index():
 
 @app.route('/todos', methods = ['POST'])
 def todos():
-    tasks[uuid.uuid1().hex] = request.form['item-text']
+    task = Task(task=request.form['item-text'])
+    tasks[task.id] = task
     return redirect(url_for('index'))
 
 @app.route('/delete', methods = ['POST'])
 def delete_todos():
     del(tasks[request.form['id']])
+    return redirect(url_for('index'))
+
+@app.route('/toggle', methods = ['POST'])
+def toggle_todo():
+    tasks[request.form['id']].toggle()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
