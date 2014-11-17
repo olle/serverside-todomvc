0565ad8cce9c6699f840abe98aaa6149142fb108
Olle Törnström <olle@studiomediatech.com>
2014-11-17 20:39:45 +0100

    Add delete capability
    * Wow, we're moving - with a true bad-practice global state as
      `database` for the todos, we can now post and store as well
      as remove todos from a little dict. Yay!

--- examples-python-flask/todomvc.py
+++ examples-python-flask/todomvc.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import uuid
+
+# Global state, yay!
+tasks = {}
+
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
-def index(name=None):
-    return render_template('index.html', name=name)
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/todos', methods = ['POST'])
+def todos():
+    tasks[uuid.uuid1().hex] = request.form['item-text']
+    return redirect(url_for('index'))
+
+@app.route('/delete', methods = ['POST'])
+def delete_todos():
+    del(tasks[request.form['id']])
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
