ad7889b63e6912541f951bc100106a81fe07f1ab
Olle Törnström <olle@studiomediatech.com>
2016-03-06 21:33:21 +0100

    Add initial skeleton app
    * Scaffold is there, an app, maven can build and shader plugin
      packs a runnable JAR. Ready for the next step!

---
+++ examples-java-native/README.md
@@ -0,0 +1,7 @@
+
+To run this project, why don't you just:
+
+   > mvn package
+   > java -jar target/examples-java-native.jar
+
+That's it.
---
+++ examples-java-native/src/main/java/com/studiomediatech/serverside/todomvc/TodoMVC.java
@@ -0,0 +1,7 @@
+package com.studiomediatech.serverside.todomvc;
+
+public class TodoMVC {
+	public static void main(String[] args) {
+		System.out.println("HELLO, BYE!");
+	}
+}
