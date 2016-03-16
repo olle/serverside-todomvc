dcfb06e91b3a4bb0a58172f438ac8fb85fd2ce9c
Olle Törnström <olle@studiomediatech.com>
2016-03-16 21:25:47 +0100

    Split the handler a bit

--- examples-java-native/src/main/java/com/studiomediatech/serverside/todomvc/TodoMVC.java
+++ examples-java-native/src/main/java/com/studiomediatech/serverside/todomvc/TodoMVC.java
@@ -33,15 +33,12 @@ public class TodoMVC {
 				InputStream in = socket.getInputStream();
 				InputStreamReader stream = new InputStreamReader(in);
 				BufferedReader buffer = new BufferedReader(stream);
-				String line = buffer.readLine();
-				System.out.println(Arrays.asList(line.split(" ")));
 				OutputStream out = socket.getOutputStream();
-				out.write(("" //
-						+ "HTTP/1.1 200 OK\n" //
-						+ "Content-Type: text/plain\n" //
-						+ "Content-Length: 13\n" //
-						+ "Connection: close\n\n" //
-						+ "hello world\n\n").getBytes());
+				{
+					Request request = parseRequest(buffer);
+					Response response = handle(request);
+					writeResponse(response, out);
+				}
 				out.flush();
 				buffer.close();
 				stream.close();
@@ -51,6 +48,33 @@ public class TodoMVC {
 			socket.close();
 		}
 
+	}
+
+	private static Request parseRequest(BufferedReader buffer) throws IOException {
+		String line = buffer.readLine();
+		System.out.println(line);
+		return null;
+	}
+
+	private static Response handle(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void writeResponse(Response response, OutputStream out) throws IOException {
+		out.write(("" //
+				+ "HTTP/1.1 200 OK\n" //
+				+ "Content-Type: text/plain\n" //
+				+ "Content-Length: 13\n" //
+				+ "Connection: close\n\n" //
+				+ "hello world\n\n").getBytes());
+	}
+
+	static final class Request {
+
+	}
+
+	static final class Response {
 	}
 
 }
