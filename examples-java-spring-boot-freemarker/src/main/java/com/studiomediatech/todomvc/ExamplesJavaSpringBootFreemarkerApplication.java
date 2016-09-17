97079f815a042de4e7dfdd6b555b4b9a182e7718
Olle Törnström <olle@studiomediatech.com>
2016-09-17 13:18:19 +0200

    Add initial runnable skeleton
    * Undertow instead of Tomcat and the initial static templates and
      CSS is there.

---
+++ examples-java-spring-boot-freemarker/src/main/java/com/studiomediatech/todomvc/ExamplesJavaSpringBootFreemarkerApplication.java
@@ -0,0 +1,12 @@
+package com.studiomediatech.todomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesJavaSpringBootFreemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesJavaSpringBootFreemarkerApplication.class, args);
+	}
+}
---
+++ examples-java-spring-boot-freemarker/src/test/java/com/studiomediatech/todomvc/ExamplesJavaSpringBootFreemarkerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.studiomediatech.todomvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamplesJavaSpringBootFreemarkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
