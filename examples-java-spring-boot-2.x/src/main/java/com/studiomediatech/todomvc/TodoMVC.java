40fd24954327816045a8d288a5661bcd5a41e474
Olle Törnström <toernstroem@synyx.de>
2018-03-17 10:01:24 +0100

    Skeleton class for Spring Boot 2.x

--- examples-java-spring-boot-2.x/src/main/java/com/studiomediatech/todomvc/TodoMVC.java
+++ examples-java-spring-boot-2.x/src/main/java/com/studiomediatech/todomvc/TodoMVC.java
@@ -3,23 +3,26 @@ package com.studiomediatech.todomvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 class TodoMVC {
+
+    @GetMapping("/todos")
+    @ResponseBody
+    String todos() {
+
+        return "Hello World!";
+    }
+
 
     public static void main(String[] args) {
 
         SpringApplication.run(TodoMVC.class, args);
-    }
-
-
-    @GetMapping("/todos")
-    String todos() {
-
-        return "Hello World!";
     }
 }
