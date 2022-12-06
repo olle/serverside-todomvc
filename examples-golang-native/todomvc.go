7bf17f3fa28d3198452e3b96651ba46f1827a4e9
Olle Törnström <olle@studiomediatech.com>
2022-12-06 18:14:06 +0100

    Serve static template

--- examples-golang-native/todomvc.go
+++ examples-golang-native/todomvc.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
