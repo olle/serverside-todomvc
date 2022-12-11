55cfff28878977b54ededf3d450ab57bcb48e9ec
Olle Törnström <olle@studiomediatech.com>
2022-12-11 08:52:40 +0100

    Template and data

--- examples-golang-native/todomvc.go
+++ examples-golang-native/todomvc.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	_ "embed"
 	"log"
 	"net/http"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
+//go:embed index.html
+var indexTemplate string
 
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		template, err := template.New("index").Parse(indexTemplate)
+
+		if err != nil {
+			w.WriteHeader(500)
+			log.Fatal(err)
+			return
+		}
+
+		w.WriteHeader(200)
+
+		data := struct {
+			ActiveCount int
+		}{
+			ActiveCount: 0,
+		}
+		template.Execute(w, data)
+	})
+	err := http.ListenAndServe("127.0.0.1:8000", mux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
