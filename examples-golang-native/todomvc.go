5f49b8834eb695c04bef0e2e7ff0a096a1ab768b
Olle Törnström <olle@studiomediatech.com>
2022-12-15 22:32:58 +0100

    Handle complete todo

--- examples-golang-native/todomvc.go
+++ examples-golang-native/todomvc.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 	"time"
 )
@@ -15,33 +16,50 @@ var indexTemplate string
 var seq = time.Now().UnixMilli()
 
 type Todo struct {
-	Id        int64
-	Text      string
-	Completed bool
-	Editing   bool
+	Id      int64
+	Text    string
+	Editing bool
 }
 
 type Data struct {
+	Show           bool
 	ActiveCount    int
+	CompletedCount int
 	ActiveTodos    []Todo
 	CompletedTodos []Todo
 }
 
-var data = Data{
-	0,
-	[]Todo{},
-	[]Todo{}}
+var data = Data{true, 0, 0, []Todo{}, []Todo{}}
+
+func updateCounts(data Data) Data {
+	data.ActiveCount = len(data.ActiveTodos)
+	data.CompletedCount = len(data.CompletedTodos)
+	return data
+}
 
 func addTodo(text string, data Data) Data {
 	seq = seq + 1
-	var NewTodo = Todo{seq, text, false, false}
-	return addNewTodo(NewTodo, data)
+	var NewTodo = Todo{seq, text, false}
+	data.ActiveTodos = append(data.ActiveTodos, NewTodo)
+	return updateCounts(data)
 }
 
-func addNewTodo(todo Todo, data Data) Data {
-	data.ActiveTodos = append(data.ActiveTodos, todo)
-	data.ActiveCount = len(data.ActiveTodos)
-	return data
+func completeTodo(id string, data Data) Data {
+	todoId, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var NewActiveTodos = []Todo{}
+	for i := 0; i < len(data.ActiveTodos); i++ {
+		todo := data.ActiveTodos[i]
+		if todo.Id == todoId {
+			data.CompletedTodos = append(data.CompletedTodos, todo)
+		} else {
+			NewActiveTodos = append(NewActiveTodos, todo)
+		}
+	}
+	data.ActiveTodos = NewActiveTodos
+	return updateCounts(data)
 }
 
 func main() {
@@ -65,10 +83,15 @@ func main() {
 				fmt.Fprintf(w, "ParseForm() err: %v", err)
 				return
 			}
-			log.Printf("HANDLING: r.PostForm = %v - r.URL = %v\n", r.PostForm, r.URL)
+			log.Printf("HANDLING: r.URL=%v r.PostForm=%v\n", r.URL, r.PostForm)
 			switch r.URL.Path {
 			case "/todos":
 				data = addTodo(r.FormValue("todo"), data)
+
+			case "/todo":
+				if r.PostForm.Has("complete") {
+					data = completeTodo(r.FormValue("complete"), data)
+				}
 			}
 			http.Redirect(w, r, "/", 301)
 
