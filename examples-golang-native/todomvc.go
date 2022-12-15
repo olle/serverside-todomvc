54a03f10ae3cf951fcf8eb9397de1736ec63fb54
Olle Törnström <olle@studiomediatech.com>
2022-12-15 21:41:31 +0100

    Add post handling cases

--- examples-golang-native/todomvc.go
+++ examples-golang-native/todomvc.go
@@ -12,39 +12,60 @@ import (
 var indexTemplate string
 
 type Todo struct {
-	Text string
+	Text      string
+	Completed bool
 }
 
 type Data struct {
-	ActiveCount int
-	Todos       []Todo
+	ActiveCount    int
+	ActiveTodos    []Todo
+	CompletedTodos []Todo
 }
 
-func main() {
+var data = Data{
+	0,
+	[]Todo{},
+	[]Todo{}}
 
-	var data = Data{0, []Todo{}}
+func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		switch r.Method {
 		case "GET":
+
 			template, err := template.New("index").Parse(indexTemplate)
+
 			if err != nil {
 				w.WriteHeader(500)
 				log.Fatal(err)
 				return
 			}
+
+			// TODO: Move this to the POST handling!!
+			data.ActiveCount = len(data.ActiveTodos)
+
 			w.WriteHeader(200)
 			template.Execute(w, data)
+
 		case "POST":
-			// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+
 			if err := r.ParseForm(); err != nil {
 				fmt.Fprintf(w, "ParseForm() err: %v", err)
 				return
 			}
-			log.Printf("Post from website! r.PostFrom = %v\n", r.PostForm)
-			data.Todos = append(data.Todos, Todo{r.FormValue("todo")})
+
+			log.Printf("HANDLING: r.PostForm = %v - r.URL = %v\n", r.PostForm, r.URL)
+
+			switch r.URL.Path {
+			case "/todos":
+
+				data.ActiveTodos = append(data.ActiveTodos, Todo{r.FormValue("todo"), false})
+
+			default:
+				log.Print("Nothing to do...")
+			}
 			http.Redirect(w, r, "/", 301)
 		default:
 			fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
