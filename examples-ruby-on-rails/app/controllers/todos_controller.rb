8d23cdf9a78e0ee20114cf7e34a9cda78f581bd1
Olle Törnström <toernstroem@synyx.de>
2022-12-23 18:45:28 +0100

    Handle create new

--- examples-ruby-on-rails/app/controllers/todos_controller.rb
+++ examples-ruby-on-rails/app/controllers/todos_controller.rb
@@ -2,4 +2,13 @@ class TodosController < ApplicationController
   def index
     @active = Todo.all
   end
+  
+  def create
+    
+    @todo = Todo.new(todo:params[:todo])
+    if @todo.save
+      redirect_to root_path
+    end
+  end
+
 end
--- examples-ruby-on-rails/config/routes.rb
+++ examples-ruby-on-rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'todos#index'
+  post "/todos", to: 'todos#create', as: 'create_todo'
 end
