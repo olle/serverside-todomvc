b982a34d41dafca3d7f50f9e15ef3c00a3e2a6c2
Olle Törnström <toernstroem@synyx.de>
2022-12-23 12:03:46 +0100

    Remove debug

--- examples-java-javalin/src/main/java/todomvc/TodoMVC.java
+++ examples-java-javalin/src/main/java/todomvc/TodoMVC.java
@@ -11,16 +11,14 @@ public class TodoMVC {
 
         JavalinJte.init();
 
-        var app = Javalin.create(config -> {
-                    config.staticFiles.add("/public");
-                    config.plugins.enableDevLogging();
-                }).start(8080);
+        var app = Javalin
+                .create(config -> config.staticFiles.add("/public"))
+                .start(8080);
 
         app.get("/", IndexPage::show);
-
+        app.post("/todo", Todos::handleTodo);
         app.post("/todos", Todos::handleTodos);
         app.post("/todos/{id}", Todos::handleTodo);
-        app.post("/todo", Todos::handleTodo);
         app.post("/controls", Todos::handleControls);
     }
 }
