97e775cc56ae0f02019e95bcc4a245012507f571
Olle Törnström <toernstroem@synyx.de>
2022-12-23 17:37:59 +0100

    Initial route

--- examples-ruby-on-rails/config/routes.rb
+++ examples-ruby-on-rails/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'todos#show'
 end
