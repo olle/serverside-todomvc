dc7e5241dd83fc98af7e5bc2f06aee20c278394f
Olle Törnström <olle@studiomediatech.com>
2023-02-12 21:51:06 +0100

    Add unique id by sec/micro epoch

--- examples-perl-cgi/cgi-bin/todo-list.pl
+++ examples-perl-cgi/cgi-bin/todo-list.pl
@@ -10,13 +10,14 @@ print "Content-Type: text/plain; charset=utf-8\n\n";
 my $PWD = getcwd();
 open(FH, '<', "$PWD/todos.dat") or die $!;
 while(<FH>){
-   my $todo = decode_base64($_);
+   my ($id,$value) = split(/\|/, $_);
+   my $todo = decode_base64($value);
    $todo =~ s/</&lt;/g;
    my $fragment = <<"END_FRAGMENT";
 <li>
-   <button name="complete" value="{todo-id}" form="todo-item" title="Mark completed"></button>
-   <button name="edit" value="{todo-id}" form="todo-item" title="Click to edit">${todo}</button>
-   <button name="delete" value="{todo-id}" form="todo-item" title="Delete todo item">&#x2715;</button>
+   <button name="complete" value="${id}" form="todo-item" title="Mark completed"></button>
+   <button name="edit" value="${id}" form="todo-item" title="Click to edit">${todo}</button>
+   <button name="delete" value="${id}" form="todo-item" title="Delete todo item">&#x2715;</button>
 </li> 
 END_FRAGMENT
    print "$fragment\n";
--- examples-perl-cgi/cgi-bin/todos.pl
+++ examples-perl-cgi/cgi-bin/todos.pl
@@ -5,6 +5,8 @@ use utf8;
 use Cwd;
 use MIME::Base64;
 use URI::Encode;
+use Time::HiRes qw(gettimeofday);
+
 my $uri = URI::Encode->new( { encode_reserved => 0 } );
 
 my $REQUEST_BODY;
@@ -15,21 +17,19 @@ $REQUEST_BODY = $uri->decode($REQUEST_BODY);
 print STDERR "REQUEST_BODY: $REQUEST_BODY\n";
 print STDERR "REQUEST_URI: $ENV{'REQUEST_URI'}\n";
 print STDERR "QUERY_STRING: $ENV{'QUERY_STRING'}\n";
-
-
 my ($key, $value) = split(/\=/, $REQUEST_BODY);
 print STDERR "FORM_VALUE: $key --> $value\n";
 
+
+
+
+my ($s, $ms) = gettimeofday();
 my $output = encode_base64($value);
+
 my $PWD = getcwd();
 open(OUT, '>>', "$PWD/todos.dat") or die $!;
-print OUT "$output";
+print OUT "$s$ms|$output";
 close(OUT);
-
-# print STDERR "PWD: $PWD\n";
-# foreach my $key (keys %ENV) {
-#     print STDERR "$key --> $ENV{$key}\n";
-# }
 
 print "Status: 301 Moved Permanently\n";
 print "Location: /\n\n";
