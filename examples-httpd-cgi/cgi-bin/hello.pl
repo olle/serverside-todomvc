4b7f4dbdbc9e0a2bb46dc2534a78e965b7d3d4b4
Olle Törnström <olle@studiomediatech.com>
2023-02-12 13:18:05 +0100

    Extract footer and use SSI

--- examples-httpd-cgi/cgi-bin/hello.pl
+++ examples-httpd-cgi/cgi-bin/hello.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
 
-print "Content-Type: text/plain; charset=utf-8\n";
-print "\n";
-print "Hello World!\n";
+print "Content-Type: text/plain; charset=utf-8\n\n";
 
+foreach my $key (keys %ENV) {
+    print "$key --> $ENV{$key}<br />";
+}
